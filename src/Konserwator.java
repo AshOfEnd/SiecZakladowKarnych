2b3084f50a678f8908c771aa9b719718e5a0f52d
Laskawy <laskawy@op.pl>
2021-12-21 01:57:02 +0100

    dodane konstruktory

--- src/Konserwator.java
+++ src/Konserwator.java
@@ -1,5 +1,9 @@
 public class Konserwator extends Pracownik
 {
+    public Konserwator(String imie, String nazwisko) {
+        super(imie, nazwisko);
+    }
+
     public void zglosZamowienie() {}
     public void zlozRaport() {}
     public void odczytajZgloszenie() {}
